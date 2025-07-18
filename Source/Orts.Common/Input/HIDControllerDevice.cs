21bcb2bb89198e8b2306b80d05aa09636119f7dc
Lukáš Belán <lukasbelan27@gmail.com>
2025-07-18 11:56:46 +0200

    Integration of HIDSharp into openrails application. Stuff added in this commit: - Stubs and skeleton for InputState and InputDevice - Reading HID reports from the device - The values are currently displayed only in console as Trace.Information, no working controls yet

---
+++ Source/Orts.Common/Input/HIDControllerDevice.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using HidSharp;
+using Microsoft.Xna.Framework.Input;
+using SharpDX.XAudio2;
+
+// This class will probabyl handle communication and parsing into usable data something.
+// I will probably create a separate class that will be returned to the UserInputHIDState where it will be used, probably
+
+namespace ORTS.Common.Input
+{
+    public class HIDControllerDevice
+    {
+        private static HIDControllerDevice instance;
+
+        public HidStream Stream { get; private set; }
+        public HidDevice Device { get; private set; }
+
+        private const int VendorId = 0xCAFE;
+        private const int ProductId = 0x4000;
+
+        public bool Enabled { set; get; } = false;
+
+        public static HIDControllerDevice Instance
+        {
+            get
+            {
+                if (null == instance)
+                {
+                    instance = new HIDControllerDevice();
+                }
+                return instance;
+            }
+        }
+
+        private HIDControllerDevice()
+        {
+            Enabled = false;
+        }
+
+        public bool Initialize()
+        {
+            var deviceList = DeviceList.Local;
+            Device = deviceList.GetHidDevices(VendorId, ProductId).FirstOrDefault();
+
+            if (Device == null)
+            {
+                return false;
+            }
+
+            if (!Device.TryOpen(out HidStream stream))
+            {
+                return false;
+            }
+
+            Stream = stream;
+            Stream.ReadTimeout = 3000;
+            Enabled = true;
+
+            return true;
+        }
+
+        public HIDDeviceReport ReadInput()
+        {
+            byte[] inputReport = new byte[HIDDeviceReport.ReportSize];
+
+            if (!Enabled || Stream == null)
+            {
+                // Dont raise an exception. In this case the device is probably just not active
+                return null;
+            }
+
+            try
+            {
+                int bytesRead = Stream.Read(inputReport, 0, HIDDeviceReport.ReportSize);
+
+                if (bytesRead != HIDDeviceReport.ReportSize)
+                {
+                    throw new IOException($"Unexpected report size: {bytesRead} instead of {HIDDeviceReport.ReportSize}");
+                }
+
+                HIDDeviceReport report = new HIDDeviceReport
+                {
+                    ButtonState = (inputReport[3] & 0x01) == 1,
+                    AxisThrottle = (UInt16)(inputReport[1] | (inputReport[2] << 8))
+                };
+                Trace.TraceInformation($"HID Input: ButtonState={report.ButtonState}, AxisThrottle={report.AxisThrottle}");
+                return report;
+            }
+            catch (Exception e)
+            {
+                System.Windows.Forms.MessageBox.Show($"Error reading HID input: {e.Message}", "HID Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+    }
+
+    public class HIDDeviceReport
+    {
+        public static int ReportSize = 4;
+        public bool ButtonState { get; set; }
+        public UInt16 AxisThrottle { get; set; }
+        public HIDDeviceReport() {}
+    }
+}
---
+++ Source/RunActivity/Viewer3D/UserInputHID.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Diagnostics;
+using Orts.Formats.Msts;
+using ORTS.Settings;
+using Orts.Viewer3D.Processes;
+using ORTS.Common.Input;
+using System.Windows.Forms;
+
+
+namespace Orts.Viewer3D
+{
+    public class UserInputHIDState : ExternalDeviceState
+    {
+        private readonly HIDControllerDevice Device;
+
+        public UserInputHIDState(Game game)
+        {
+            Device = HIDControllerDevice.Instance;
+
+            // Display information that the device is not enable, probably failed to connect
+            if (Device is null || !Device.Initialize())
+            {
+                MessageBox.Show("HID Controller not enabled. Please check your settings.", "HID Controller Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+        public void Update()
+        {
+            if (Device is null || !Device.Enabled)
+            {
+                return; // Device not initialized or not enabled
+            }
+            HIDDeviceReport report = Device.ReadInput();
+        }
+        public void Activate()
+        {
+    
+        }
+
+        private static float Percentage(float x, float x0, float x100)
+        {
+            float p = 100 * (x - x0) / (x100 - x0);
+            if (p < 0)
+                return 0;
+            if (p > 100)
+                return 100;
+            return p;
+        }
+
+        private static float Percentage(byte value, (byte p0, byte p100) range)
+        {
+            float p = 100 * (value - range.p0) / (range.p100 - range.p0);
+            if (p < 0)
+                return 0;
+            if (p > 100)
+                return 100;
+            return p;
+        }
+
+        private static float Percentage(byte value, (byte p100Minus, byte p0, byte p100Plus) range)
+        {
+            float p = 100 * (value - range.p0) / (range.p100Plus - range.p0);
+            if (p < 0)
+                p = 100 * (value - range.p0) / (range.p0 - range.p100Minus);
+            if (p < -100)
+                return -100;
+            if (p > 100)
+                return 100;
+            return p;
+        }
+
+        private (byte, byte) UpdateCutOff((byte, byte) range, byte cutOff)
+        {
+            if (range.Item1 < range.Item2)
+            {
+                range.Item1 += cutOff;
+                range.Item2 -= cutOff;
+            }
+            else
+            {
+                range.Item2 += cutOff;
+                range.Item1 -= cutOff;
+            }
+            return range;
+        }
+
+        private (byte, byte, byte) UpdateCutOff((byte, byte, byte) range, byte cutOff)
+        {
+            if (range.Item1 < range.Item3)
+            {
+                range.Item1 += cutOff;
+                range.Item3 -= cutOff;
+            }
+            else
+            {
+                range.Item3 += cutOff;
+                range.Item1 -= cutOff;
+            }
+            return range;
+        }
+
+        public bool Active { get; private set; }
+    }
+
+    public class HIDButton : ExternalDeviceButton
+    {
+        int Index;
+        byte Mask;
+        public HIDButton(byte button)
+        {
+            Index = 8 + button / 8;
+            Mask = (byte)(1 << (button % 8));
+        }
+        public void Update(byte[] data)
+        {
+            IsDown = (data[Index] & Mask) != 0;
+        }
+    }
+
+}
+
